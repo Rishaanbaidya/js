295bb9835158c3517d2f5704670a858859d93ac1
Rishaanbaidya <95754309+Rishaanbaidya@users.noreply.github.com>
2024-07-09 16:28:21 +0000

    02 data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+//  alert( 3+3) // we are using nodejs,not the browser
+console.log()
+console.log(Rishaan);
+
+
+
+let name = "Rishaan"
+let age = 9
+let isLoggedIn = true
+
+
+//  number =>"2 to power 53"
+//  bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);// object
