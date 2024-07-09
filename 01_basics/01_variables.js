cdc99fdbc51b46cf005893566d93e7aafc36abe6
Rishaanbaidya <95754309+Rishaanbaidya@users.noreply.github.com>
2024-07-09 16:27:22 +0000

    variabbles and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "rishaan.baidya12@gmail.com"
+var accountPassword = "Ih@teyou1"
+accountCity = "Bangkok"
+let accountState;
+
+// accountId = 2 //not allowed
+
+
+accountEmail = "ehheej12@gmail.com"
+accountPassword = "123456789"
+accountCity = "kolkata"
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+
+/*Prefer not to use var
+because of issue inblock scope and functional scope
+*/
+
+
+
+
